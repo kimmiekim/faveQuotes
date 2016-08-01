d2f8ff0c027b375a06c117150d3d0a19ad4ccbf4
Erin Kim <kim.erin.kim@gmail.com>
2016-08-01 08:15:26 -0400

    First commit

---
+++ app/controllers/quotes_controller.rb
@@ -0,0 +1,26 @@
+class QuotesController < ApplicationController
+  def index
+    @quotes = Quote.all
+    @quote = Quote.new
+  end
+
+  def create
+    @quote = Quote.new(quote_params)
+    respond_to do |format|
+      if @quote.save
+        format.html
+        format.js{}
+      else
+        format.html
+      end
+    end
+  end
+
+  def destroy
+  end
+
+  private
+  def quote_params
+    params.require(:quote).permit(:content)
+  end
+end
---
+++ db/migrate/20160731210221_create_quotes.rb
@@ -0,0 +1,10 @@
+class CreateQuotes < ActiveRecord::Migration
+  def change
+    create_table :quotes do |t|
+      t.string :write
+      t.string :content
+
+      t.timestamps null: false
+    end
+  end
+end
